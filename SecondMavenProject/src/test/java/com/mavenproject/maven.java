bc85e13f5f76152d2ad73e319bade34c90006e2d
Arjun reddy kallem <Arjun reddy kallem@192.168.0.5>
2018-09-01 08:51:23 +0530

    new project

---
+++ SecondMavenProject/src/test/java/com/mavenproject/maven.java
@@ -0,0 +1,14 @@
+package com.mavenproject;
+
+import org.testng.annotations.Test;
+
+public class maven {
+	
+	@Test
+	
+	public void print() {
+		
+		System.out.println("print here");
+	}
+
+}
---
+++ SecondMavenProject/src/test/java/com/secondproject/SecondProjectTest.java
@@ -0,0 +1,113 @@
+package com.secondproject;
+
+import static org.testng.Assert.assertEquals;
+
+import org.apache.commons.lang.RandomStringUtils;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.Select;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+
+public class SecondProjectTest {
+
+	WebDriver driver;
+
+	@BeforeTest
+	public void openBrowser() {
+
+		System.setProperty("webdriver.chrome.driver", "C:\\Users\\Arjun reddy kallem\\Downloads\\chromedriver_win32\\chromedriver.exe");
+		driver = new ChromeDriver();
+
+	}
+
+	@Test
+	public void method() throws InterruptedException {
+
+
+		//enter url using driver class and get method
+		driver.get("http://www.automationpractice.com");
+
+		driver.findElement(By.className("login")).click();
+
+		driver.findElement(By.id("email_create")).sendKeys(RandomStringUtils.randomAlphabetic(5)+"@mailinator.com");
+
+		//element3.sendKeys("k.mreddy9116@gmail.com");
+
+
+		driver.findElement(By.id("SubmitCreate")).click();;
+
+
+		//to slow down the process speed
+		Thread.sleep(5000);;
+
+		driver.findElement(By.id("uniform-id_gender1")).click();
+
+		driver.findElement(By.id("customer_firstname")).sendKeys("arjunreddy");
+
+		driver.findElement(By.id("customer_lastname")).sendKeys("kallem");
+
+		driver.findElement(By.id("passwd")).sendKeys("Mallik12345");
+
+		WebElement DaysDropdown = driver.findElement(By.id("days"));
+
+
+		//Select is the class created mainly for dropdown
+
+		new Select(DaysDropdown).selectByValue("29");;
+
+		WebElement monthdropdown = driver.findElement(By.id("months"));
+
+		new Select(monthdropdown).selectByValue("12");
+
+		WebElement yeardropdown = driver.findElement(By.id("years"));
+
+		new Select (yeardropdown).selectByValue("1989");
+
+		driver.findElement(By.id("newsletter")).click();
+
+		driver.findElement(By.id("company")).sendKeys("american express");
+
+		driver.findElement(By.id("address1")).sendKeys("Fortlauderdale , FL");
+
+		driver.findElement(By.id("city")).sendKeys("Sunrise");
+
+		WebElement Statedropdown = driver.findElement(By.id("id_state"));
+
+		new Select (Statedropdown).selectByValue("9");
+
+		driver.findElement(By.id("postcode")).sendKeys("12345");
+
+		driver.findElement(By.id("phone_mobile")).sendKeys("9194485338");
+
+
+		driver.findElement(By.id("alias")).sendKeys(" charolette,NC");
+
+		driver.findElement(By.id("submitAccount")).click();
+
+		Thread.sleep(3000);
+
+
+		String actualresult = driver.findElement(By.xpath("//p[@class='info-account']")).getText();
+
+		System.out.println("actualresult ::"+actualresult);
+
+		String expectedresult = "Welcome to your account. Here you can manage all of your personal information and orders.";
+
+		System.out.println("expectedresult ::"+expectedresult);
+
+		/*	if(actualresult.contains(expectedresult)) {
+			System.out.println("Test case pass");
+		}
+		else {
+			System.out.println("Test case fail");
+		}*/
+
+		assertEquals(actualresult,expectedresult);
+		driver.quit();
+	}
+
+
+}
